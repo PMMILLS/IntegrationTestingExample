366e59668bea17e81fe80e3f93f5af706ff867fb
Philip Mills <pm.mills@icloud.com>
2020-03-25 20:41:40 +0000

    first commit

---
+++ IntegrationTestingExample/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IntegrationTestingExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Hello", "World", "!"
+        };
+
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Summaries;
+        }
+    }
+}
---
+++ IntegrationTestingExampleTests/Controllers/TestControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IntegrationTestingExample;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace IntegrationTestingExampleTests.Controllers
+{
+    [TestClass]
+    public class TestControllerTests
+    {
+        private HttpClient _client;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _client = new CustomWebApplicationFactory<Startup>().CreateClient();
+        }
+
+        [TestMethod]
+        public async Task Verify_ResultFrom_Client()
+        {
+            // The endpoint or route of the controller action.
+            HttpResponseMessage httpResponse = await _client.GetAsync("/test");
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Deserialize and examine results.
+            string stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            List<string> result = JsonConvert.DeserializeObject<List<string>>(stringResponse);
+
+            // Assert some stuff
+            Assert.AreEqual("Hello", result[0]);
+            Assert.AreEqual("World", result[1]);
+            Assert.AreEqual("!", result[2]);
+        }
+
+        [TestMethod]
+        public async Task Verify_ClientCall_IsSuccess()
+        {
+            // The endpoint or route of the controller action.
+            HttpResponseMessage httpResponse = await _client.GetAsync("/test");
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Assert some stuff
+            Assert.IsTrue(httpResponse.IsSuccessStatusCode);
+        }
+    }
+}
